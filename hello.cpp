5c454379c8160b9ad119d2d1aaf3fb2335becb38
anurag <anurags153@gmail.com>
2019-05-08 20:27:34 +0530

    first

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char name[20];
+    cout<<"Enter name";
+    cin>>name;
+    cout<<"\n hello"<<name;
+    cin.ignore();
+    cin.get();
+    return 0;
+}
